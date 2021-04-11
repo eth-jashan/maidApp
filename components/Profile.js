eff7a03573aa2495b944ceb5ccc739c37aa58f78
Piyush <piyushjb7@hotmail.com>
2021-04-11 19:25:34 +0530

    maid registration done

--- components/Profile.js
+++ components/Profile.js
@@ -8,39 +8,71 @@ import TimePicker from 'react-native-modal-datetime-picker';
 //icons
 import { AntDesign } from '@expo/vector-icons'; 
 import { MaterialIcons } from '@expo/vector-icons'; 
-import { add, set } from 'react-native-reanimated';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { Feather } from '@expo/vector-icons'; 
+//components 
+import RNPickerSelect from 'react-native-picker-select';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 
-//components
-import Card from '../components/Card';
+
+
 
 const Profile = props => {
-
     const[name,setName] = useState('');
     const[phone,setPhone] = useState('');
     const[address,setAdress] = useState('');
     const[time,setTime] = useState('');
     const[toTime,setToTime] = useState('');
+    const[basePrice,setBasePrice] = useState('');
+    const[work,setWork] = useState('');
+    const[workArray,setWorkArray] = useState([]);
    
     const[clockVisible,setClockVisible] = useState(false);
     const[Visible,setVisible] = useState(false);
     const[modalVisible,setModalVisible] = useState(false);
 
     const[load,setLoad] =  useState(false)
+    
+
+    const addWork = () => {
+        if(workArray.includes(work)){
+            Alert.alert('Already Exists','The Work is already chosen',[{text:'Okay',onPress:()=>setModalVisible(false)}])
+        }  
+        else{
+            setWorkArray(currentWork =>[...currentWork,work]);
+            console.log(workArray);
+            setModalVisible(false);
+            setWork(null);
+        }
+
+    };
+
+    const cancelWork = () => {
+        setModalVisible(false);
+        setWork(null);
+    };
+
+
 
     const reset = () => {
         setName('');setPhone('');setAdress('');setTime('');setToTime('');
         setClockVisible(null);setVisible(null);setModalVisible(null);
-        setLoad(false);
+        setLoad(false);setWorkArray([]);setBasePrice(null);
         console.log(name,phone,address,time,toTime)
     };
 
-    const validate = (name,phone,address,time,toTime) => {
-        if(name.trim().length > 0 && phone.trim().length > 0  && time.trim().length > 0 &&toTime.trim().length > 0){
-            console.log('work nigga')
-        }else{
+    const validate = () => {
+        if(name.trim().length > 0 && phone.trim().length > 0  && time.trim().length > 0 &&
+          toTime.trim().length > 0 && basePrice.trim().length > 0 && workArray.length!=0 ){
+            console.log('work')
+            
+        }
+        else{
             Alert.alert('Invalid Form Data','Please Enter all The required Data',[{text:'Okay'}])
         }
     }
+
+
 
     const showTimePicker = () =>{
         setClockVisible(true);
@@ -71,9 +103,10 @@ const Profile = props => {
 
     return(
         <SafeAreaView styles={styles.maincontainer} >
+            <ScrollView>
             <View style={{width:Dimensions.get('window').width, alignSelf:'center'} }>
             <View style={{margin:10,justifyContent:'center',alignItems:'center'}}>
-            <Text style={{margin:30,color:'#e2703a',fontSize:25,fontWeight:'bold'}} >Register</Text>
+            <Text style={{margin:30,color:'#e2703a',fontSize:25,fontWeight:'bold'}} >Maid Registration</Text>
             <View>
                 <TextInput
                 mode='outlined'
@@ -99,6 +132,15 @@ const Profile = props => {
                 onChangeText={text=>setAdress(text)}
                 theme={{colors:{primary:"#ba8f54",underlineColor:'transparent'}}}
                 label='Enter Address(Optional)'
+                />
+                 <TextInput
+                mode='outlined'
+                style={styles.input}
+                value={basePrice}
+                keyboardType='phone-pad'
+                onChangeText={text=>setBasePrice(text)}
+                theme={{colors:{primary:"#ba8f54",underlineColor:'transparent'}}}
+                label='Your Base Price'
                 />
             </View>
             {/* clock 1 */}
@@ -147,6 +189,13 @@ const Profile = props => {
             </View>
             
             </View>
+            <View style={{alignItems:'center',justifyContent:'center',flexDirection:'row'}}>
+   {/* ARRAY OF WORK */}
+                {workArray.map((kaam) => <View style={{borderWidth:1,marginHorizontal:5,padding:5,borderRadius:10}}>
+                    <Text key={kaam} style={{fontWeight:'bold',margin:2}}>{kaam}</Text>
+                    </View>)}
+                {workArray.length!=0?<Feather style={{margin:5}} name="delete" size={23} color="red"  onPress={()=>{setWorkArray([])}}/>:null}
+            </View>
             <TouchableOpacity  style={styles.button}  onPress={()=>setModalVisible(true)}>
                 {load?<ActivityIndicator color='#ffffff' size='small'/>:
                     <Text style={{fontWeight:'bold',fontSize:17,color:'#ffffff',textAlign:'center'}} >Select Work</Text>}
@@ -161,8 +210,8 @@ const Profile = props => {
                             <Text style={{fontWeight:'bold',fontSize:17,color:'#ffffff',textAlign:'center'}} >Reset</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity  style={styles.buttonAgree}  onPress={validate.bind(this,name,phone,address,time,toTime)}>
-            <Text style={{fontWeight:'bold',fontSize:17,color:'#ffffff',textAlign:'center'}} >Done</Text>
+            <TouchableOpacity  style={styles.buttonAgree}  onPress={validate}>
+            <Text style={{fontWeight:'bold',fontSize:17,color:'#ffffff',textAlign:'center'}} >Submit</Text>
             </TouchableOpacity>
             </View>
 
@@ -171,14 +220,35 @@ const Profile = props => {
             <Modal animationType='fade' transparent={true} visible={modalVisible}>
                 <View style={{flex: 1,justifyContent: "center",alignItems: "center",marginTop: 22}}>
                     <View style={styles.modal}>
-                        <Card/>
-                        <TouchableOpacity  style={styles.button}  onPress={()=>setModalVisible(false)}>
+                        
+                        <RNPickerSelect
+                        onValueChange={(value) => setWork(value)}
+                        items={[
+                         { label: 'House Cleaning', value: 'House Cleaning' },
+                         { label: 'Vehicle Wash', value: 'Vehicle Wash' },
+                         { label: 'Cooking', value: 'Cooking' },
+                              ]}
+                              style={{ inputAndroid: { color: 'black' } }}
+                        useNativeAndroidPickerStyle={true}
+                        fixAndroidTouchableBug={true}
+                        Icon={() => {
+                            return <MaterialCommunityIcons name="broom" size={24} color="black" />;
+                          }}/>
+                          <View style={{flexDirection:'column',justifyContent:'center'}} >
+                          {work?<TouchableOpacity  style={styles.buttonDone}  onPress={addWork}>
                             <Text style={{fontWeight:'bold',fontSize:17,color:'#ffffff',textAlign:'center'}} >Done</Text>
+                        </TouchableOpacity>:null}
+                              <TouchableOpacity  style={styles.buttonDisagree}  onPress={cancelWork}>
+                            <Text style={{fontWeight:'bold',fontSize:17,color:'#ffffff',textAlign:'center'}} >Cancel</Text>
                         </TouchableOpacity>
+
+                        </View>
+                        
             
                     </View>
                 </View>
             </Modal>
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -226,8 +296,9 @@ const styles = StyleSheet.create({
         backgroundColor:'#ffffff',
         margin:10,
         justifyContent:'center',
-        alignItems:'center',
-        padding:10,
+        width:Dimensions.get('window').width*0.8,
+        
+        padding:20,
         borderWidth:1,
         borderColor:'black'
     },
@@ -252,6 +323,15 @@ const styles = StyleSheet.create({
     buttonDisagree:{
         borderRadius:20,
         backgroundColor:'black',
+        width:Dimensions.get('window').width*0.40,
+        margin:10,
+        padding:10,
+        alignSelf:'center',
+        position:'relative'
+    },
+    buttonDone:{
+        borderRadius:20,
+        backgroundColor:'#e2703a',
         width:Dimensions.get('window').width*0.40,
         margin:10,
         padding:10,
